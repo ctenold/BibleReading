f85d7b857dd8ad39bb88bc487aca92c2677a4199
ctenold <88893153+ctenold@users.noreply.github.com>
2026-01-17 06:57:29 -0600

    critical fixes

--- sw.js
+++ sw.js
@@ -1,21 +1,34 @@
-const CACHE_NAME = 'bible-reading-v1';
+const CACHE_NAME = 'bible-reading-v2';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png',
-  '/plans/mcheyne.json',
-  '/plans/oneyearchronological.json',
-  '/plans/esvthroughthebible.json',
-  '/plans/esvliterarystudybible.json',
-  '/plans/heartlightotandnt.json'
+  '/BibleReading/',
+  '/BibleReading/index.html',
+  '/BibleReading/manifest.json',
+  '/BibleReading/icon-192.png',
+  '/BibleReading/icon-512.png',
+  '/BibleReading/plans/mcheyne.json',
+  '/BibleReading/plans/oneyearchronological.json',
+  '/BibleReading/plans/esvthroughthebible.json',
+  '/BibleReading/plans/esvliterarystudybible.json',
+  '/BibleReading/plans/heartlightotandnt.json',
+  '/BibleReading/plans/fiveday2026.json'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames
+          .filter(cacheName => cacheName !== CACHE_NAME)
+          .map(cacheName => caches.delete(cacheName))
+      );
+    })
   );
 });
 
